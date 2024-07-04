db97a96993c4d973b8a7ddedfe3785ee80c927ab
MANASARAJARAPU2002 <manasarajarapu02@gmail.com>
2024-07-04 16:53:17 +0530

    Update User_Details.java

--- User_Details.java
+++ User_Details.java
@@ -1,63 +1,48 @@
-package Capstone.Project.Team2;
+package Project.Final.UserCode;
 
-	public class User_Details {
-	    private int userId;
-	    private String name;
-	    private String email;
-	    private String password;
-	    private String phoneNumber;
-
-	    public User_Details() {
-	    	super();
-	    }
-
-	    public User_Details(String name, String email, String password, String phoneNumber, String address) {
-	        this.name = name;
-	        this.email = email;
-	        this.password = password;
-	        this.phoneNumber = phoneNumber;
-	        
-	    }
-		public int getUserId() {
-			return userId;
-		}
-
-		public void setUserId(int userId) {
-			this.userId = userId;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public String getEmail() {
-			return email;
-		}
-
-		public void setEmail(String email) {
-			this.email = email;
-		}
-
-		public String getPassword() {
-			return password;
-		}
-
-		public void setPassword(String password) {
-			this.password = password;
-		}
-
-		public String getPhoneNumber() {
-			return phoneNumber;
-		}
-
-		public void setPhoneNumber(String phoneNumber) {
-			this.phoneNumber = phoneNumber;
-		}		
-	    
+public class User {
+	private String username;
+	private String password;
+	private String email;
+	private String phoeneno;
+	
+	public User() {
+		super();
 	}
+	
+	public User(String username, String password, String email, String phoeneno) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.phoeneno = phoeneno;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	
+	public String getPassword() {
+		return password;
+	}
+
+	
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhoeneno() {
+		return phoeneno;
+	}
+
+	@Override
+	public String toString() {
+		return "Username=" + username + " \nemail=" + email + " \nphoeneno=" + phoeneno;
+	}
+
+}
+
 
 
